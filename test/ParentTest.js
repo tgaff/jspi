d01f450e3141d8ca515371eada890bd1e60b46d1
tgaff <tgaff@alumni.nd.edu>
2015-07-25 17:03:24 -0700

    Demonstrate problems and nearly true inheritance by using Object.create and calling the parent constructor

--- test/ParentTest.js
+++ test/ParentTest.js
@@ -31,7 +31,7 @@ describe('TopParent obj', function() {
 
   it('constructor objects are NOT shared', function() {
     child1.unSharedObj.x = 'asfd';
-    expect(child2.unSharedObj.x).to.equal('shared');
+    expect(child2.unSharedObj.x).to.equal('unshared');
     expect(child1.unSharedObj.x).to.equal('asfd');
   });
   describe('getPrivate has a closure', function() {
@@ -55,7 +55,7 @@ describe('TopParent obj', function() {
     expect(child2.proto_y.a).to.equal(439);
   });
   it('functions on the prototype are shared', function() {
-    expect(child1.shared_function).to.eql(child2.shared_function);
+    expect(child1.sharedFunction).to.eql(child2.sharedFunction);
     // but not in the constructor
     expect(child1.getPrivate).to.not.eql(child2.getPrivate);
   });
@@ -87,7 +87,90 @@ describe('TopParent obj', function() {
 
 
 
+describe('SubParent', function() {
+  beforeEach(function() {
+    tp1 = new subject.TopParent('tp1');
+    tp2 = new subject.TopParent('tp2');
+    sp1 = new subject.SubParent('sp1');
+    sp2 = new subject.SubParent('sp2');
+  });
+  describe('defined on the sub-class', function() {
+    it('has the instanceVar', function() {
+      expect(sp1.instanceVar).to.equal('SubParent: sp1');
+    });
+    it('instanceVar is unique per instance', function() {
+      expect(sp1.instanceVar).to.equal('SubParent: sp1');
+      expect(sp2.instanceVar).to.equal('SubParent: sp2');
+    });
+    it('instanceVar did not sneak up to the parent objects children', function() {
+      expect(tp1.instanceVar).to.be.undefined;
+    });
+  });
+});
+describe('inheritance from TopParent', function() {
+  it('can access the same private Variables (from constructor)', function() {
+    tp1.getPrivate('objWithoutThis').x.should.equal('objWithoutThis');
+    sp1.getPrivate('objWithoutThis').x.should.equal('objWithoutThis');
+    sp2.getPrivate('objWithoutThis').x.should.equal('objWithoutThis');
+  });
+  it('can use the prototype method', function() {
+    tp1.sharedFunction(3);
+    sp1.sharedFunction(10);
+    sp2.sharedFunction(100);
+
+    // NOTE the validations here are a little dumb due to the weird
+    // overriding of __proto__.proto_x in TopParent
+    expect(sp1.proto_x).to.equal(12);
+    expect(sp2.proto_x).to.equal(102);
+
+    expect(tp1.proto_x).to.equal(5);
+  });
+// HERES THE BIG ONE:
+  it("because the prototype of all subclasses is the same\
+    instance of TopParent, they share all objects created by\
+    TopParent constructor", function() {
+    // first show that top parent doesnt have this sharing issue
+    tp1.unSharedObj.x.should.equal('unshared');
+    tp2.unSharedObj.x = 'asdf';
+    tp1.unSharedObj.x.should.equal('unshared');
+
+    //the subparent DOES have the issue
+    sp1.unSharedObj.x.should.equal('unshared');
+    sp2.unSharedObj.x = 'i was set by sp2';
+
+    sp1.unSharedObj.x.should.equal('i was set by sp2');
+
+  });
 
 
 
+// Lets see how we can use Object.create to resolve this
+//
+// err first lets see if that's even true
+//
+// Previously we had
+//   SubParent.prototype = new TopParent();
+// now we use:
+//   SubparentV2.prototype = Object.create(TopParent.prototype);
+  describe('using Object.create with SubParentV2', function() {
+    beforeEach(function() {
 
+      spv2a = new subject.SubParentV2('thomas');
+      spv2b = new subject.SubParentV2();
+    });
+    it("true inheritance", function() {
+      // redemonstrate subparent's issue
+      //the subparent DOES have the issue
+      sp1.unSharedObj.x = 'foo';
+      sp1.unSharedObj.x.should.equal('foo');
+      sp2.unSharedObj.x = 'i was set by sp2';
+      sp1.unSharedObj.x.should.equal('i was set by sp2');
+      spv2a.unSharedObj.x.should.equal('unshared');
+      spv2b.unSharedObj.x = 'i was set by spv2b';
+      spv2a.unSharedObj.x.should.equal('unshared');
+
+    });
+  });
+
+
+});
